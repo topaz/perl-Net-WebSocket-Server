320c63639f0bc622476bf0512edf00850ca05e5d
Topaz <topaz@ganymede>
2013-06-06 00:52:04 -0400

    advancing to 0.1.3 for release

--- lib/Net/WebSocket/Server.pm
+++ lib/Net/WebSocket/Server.pm
@@ -10,7 +10,7 @@ use IO::Select;
 use Net::WebSocket::Server::Connection;
 use Time::HiRes qw(time);
 
-our $VERSION = '0.001002';
+our $VERSION = '0.001003';
 $VERSION = eval $VERSION;
 
 sub new {
