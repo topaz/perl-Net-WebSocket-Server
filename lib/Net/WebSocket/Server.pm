04dc48664c7e13b9e82338aa6377567a0ca8e3b1
Topaz <topaz@ganymede>
2013-10-26 13:44:06 -0400

    version 0.2.2

--- lib/Net/WebSocket/Server.pm
+++ lib/Net/WebSocket/Server.pm
@@ -10,7 +10,7 @@ use IO::Select;
 use Net::WebSocket::Server::Connection;
 use Time::HiRes qw(time);
 
-our $VERSION = '0.002001';
+our $VERSION = '0.002002';
 $VERSION = eval $VERSION;
 
 sub new {
